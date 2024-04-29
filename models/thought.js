8af87db8dbe98583a3617ddf3c7d57557e54cd0d
NatDavies06 <153016845+NatDavies06@users.noreply.github.com>
2024-04-29 20:10:30 +0000

    messed up defining my reaction schema

--- models/thought.js
+++ models/thought.js
@@ -1,34 +1,56 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-// Function for thought schema
-const thoughtSchema = new Schema({
-    thoughtText: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 280
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (createdAtVal) => dateFormat(createdAtVal)
-    },
-    username: {
-        type: String,
-        required: true
-    },
-    reactions: [reactionSchema]
-}, {
-    toJSON: {
-        virtuals: true,
-        getters: true
-    },
-    id: false
+// Define the reaction schema
+const reactionSchema = new Schema({
+  reactionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId()
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal)
+  }
 });
 
-thoughtSchema.virtual('reactionCount').get(function () {
-    return this.reactions.length;
+// Defining the thought schema
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal)
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [reactionSchema] // Embed the reaction subdocument schema
+}, {
+  toJSON: {
+    virtuals: true,
+    getters: true
+  },
+  id: false
+});
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
 });
 
 const Thought = mongoose.model('Thought', thoughtSchema);
