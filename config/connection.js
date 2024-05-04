05a7fbef779086a278dd8e33e94534b7c3cd4a50
NatDavies06 <153016845+NatDavies06@users.noreply.github.com>
2024-05-04 22:30:24 +0000

    updated connection file

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,9 @@ const { connect, connection } = require('mongoose');
 
 const connectionString = 'mongodb://127.0.0.1:27017/studentsDB';
 
-connect(connectionString);
+connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 module.exports = connection;
