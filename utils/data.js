5cc8f41e59ab05847d896e6174155674840f9ef2
NatDavies06 <153016845+NatDavies06@users.noreply.github.com>
2024-05-04 21:42:40 +0000

    updated data.js and added my twist to the social media site

---
+++ utils/data.js
@@ -0,0 +1,34 @@
+const fishNames = [
+    "Shark",
+    "Salmon",
+    "Whale",
+    "Octopus",
+    "Crab",
+    "Srhimp",
+    "Marlin",
+    "Trout",
+];
+
+const email = [
+    "jaws3d@enoughfish.com",
+    "sushiSympothizer@enoughfish.com",
+    "hMelville@enoughfish.com",
+    "possibleAlien@enoughfish.com",
+    "waterSpider@enoughfish.com",
+    "golioth@enoughfish.com",
+    "marlinAndM@enoughfish.com",
+    "larryfisherman@enoughfish.com",
+];
+
+const fishThoughts = [
+    "Just spotted a school of tasty fish nearby. Time for a feeding frenzy! #DinnerTime",
+    "Swimming upstream against the current like a boss. 💪🐟 #SalmonStrong",
+    "Just breeching the surface to say hi to all my ocean friends! 🐋💦 #BigBlue",
+    "Got my tentacles on some delicious seafood for dinner tonight. Who's joining me?",
+    "Digging into the sand for some hidden treasures. Who needs a metal detector when you have claws? 💰 #Beachcomber",
+    "Hanging out with my shrimp squad, swimming in unison like the synchronized swimmers we are! 🌊 #TeamShrimp",
+    "Just broke the surface to catch some air. It's a beautiful day out here in the open ocean! 🌊☀️ #MarlinLife",
+    "Taking cover under a shady rock, waiting for the perfect moment to strike. Patience is key in the game of survival! 🎣🐟 #TroutTactics",
+];
+
+module.exports = { fishNames, email, fishThoughts };
