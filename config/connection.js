9528bbca92f65e76d367e7ad3dd3396afb986764
NatDavies06 <153016845+NatDavies06@users.noreply.github.com>
2024-05-22 01:59:24 +0000

    connection file update.

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
-const mongoose = require("mongoose");
+const { connect, connection } = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/localhost:3001');
+const connectionString = 'mongodb://127.0.0.1:27017';
 
-module.exports = mongoose.connection;
+connect(connectionString);
+
+module.exports = connection;
